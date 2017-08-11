a50d27381d081b869b4bda52b04565e0b81d580c
fobossalmeron <fobos.salmeron@gmail.com>
2017-08-11 15:17:14 -0500

    no-php

--- server/bala.php
+++
@@ -1,9 +0,0 @@
-<?php 
-
-	$db = mysqli_connect('localhost', '9279834aa847', 'abc18b3122ba48ed', 'balafilmsdb'); //nombrehost,user,password,databasetoconnect
-	if(mysqli_connect_errno())
-	{
-		echo 'Failed to connect to MySQL: '.mysqli_connect_error();
-	}
-
- ?>
